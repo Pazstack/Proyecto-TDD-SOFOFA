fe34fade1d95f536c0e4aa928dd5d6aecb6d2865
Paz <Paz.gutierrez.portillo@gmail.com>
2025-07-07 23:41:25 -0400

    Primer commit del pruyecto TDD

---
+++ src/main/java/repository/ItemService.java
@@ -0,0 +1,9 @@
+package repository;
+public class ItemService {
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+    public int restar(int a, int b) {
+        return a - b;
+    }
+}
